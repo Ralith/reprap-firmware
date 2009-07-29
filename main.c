56c37131301dd7d81b71b0ace7683b3845ea9899
Ralith <ralith@gmail.com>
2009-07-28 20:40:04 -0700

    Additional functionality

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) 
-{
-	/* All of PORTB -> digital output mode */
-	DDRB = 0xFF;
+#include "digital.h"
+#include "serial.h"
 
+int main(void)
+{
+	ser_init();
+	dig_mode(1, OUTPUT);
+	dig_mode(19, INPUT);
+
+	digstate_t state;
 	while(1) {
-		PORTB = 0xFF;
-		_delay_ms(500);
-		PORTB = 0x00;
-		_delay_ms(500);
+		state = dig_read(19);
+		dig_write(1, state);
+		if(state == LOW) {
+			ser_write_str("LOW\r\n");
+		} else {
+			ser_write_str("HIGH\r\n");
+		}
 	}
 	return 0;
 }
--- types.h
+++ types.h
@@ -9,10 +9,31 @@ typedef enum digmode {
 } digmode_t;
 
 typedef enum digstate {
-	OFF = 0,
-	ON = 1
+	LOW = 0,
+	HIGH = 1
 } digstate_t;
 
+/* See datasheet Figure 1-1 */
+#define PIN_MIN 1
+#define PIN_PORTB_MIN PIN_MIN
+#define PIN_PORTB_MAX 8
+#define PIN_PORTD_MIN 14
+#define PIN_PORTD_MAX 21
+#define PIN_PORTC_MIN 22
+#define PIN_PORTC_MAX 29
+#define PIN_PORTA_MIN 33
+#define PIN_PORTA_MAX 40
+
 typedef unsigned char pin_t;
+
+typedef int port_t;
+typedef int ddr_t;
+
+#define TRUE 1
+#define FALSE 0
+typedef unsigned char bool;
+
+typedef signed char byte;
+typedef unsigned char ubyte;
 
 #endif
