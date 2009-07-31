07c1476652465be3fd9812ca66184bc706493843
Ralith <ralith@gmail.com>
2009-07-31 03:49:50 -0700

    Added basic stepper I/O initialization code.

--- main.c
+++ main.c
@@ -5,27 +5,21 @@
 #include "digital.h"
 #include "uart.h"
 
+#include "stepdrive.h"
+
 #define UART_BAUD_RATE 19200
 
 int main(void)
 {
+	/* Initialize stepper control */
+	stepdrive_init();
+		
 	/* Enable UART for serial comms */
 	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
 	/* Enable interrupts needed by UART lib */
 	sei();
 
-	dig_mode(1, OUTPUT);
-	/*dig_mode(24, INPUT);*/
-
-	digstate_t state;
 	for(;;) {
-		/*state = dig_read(24);
-		dig_write(1, state);
-		if(state == LOW) {
-			uart_puts("LOW\r\n");
-		} else {
-			uart_puts("HIGH\r\n");
-			}*/
 	}
 	return 0;
 }
--- stepdrive.c
+++ stepdrive.c
@@ -3,9 +3,35 @@
 #include <avr/interrupt.h>
 
 #include "digital.h"
+
 #include "pins.h"
 
 void stepdrive_init() 
 {
-	
+	dig_mode(X_STEP_PIN, OUTPUT);
+	dig_mode(X_DIR_PIN, OUTPUT);
+	dig_mode(X_ENABLE_PIN, OUTPUT);
+	dig_mode(X_MIN_PIN, INPUT);
+#ifdef X_MAX_PIN
+	dig_mode(X_MAX_PIN, INPUT);
+#endif
+	dig_write(X_ENABLE_PIN, LOW);
+
+	dig_mode(Y_STEP_PIN, OUTPUT);
+	dig_mode(Y_DIR_PIN, OUTPUT);
+	dig_mode(Y_ENABLE_PIN, OUTPUT);
+	dig_mode(Y_MIN_PIN, INPUT);
+#ifdef Y_MAY_PIN
+	dig_mode(Y_MAY_PIN, INPUT);
+#endif
+	dig_write(Y_ENABLE_PIN, LOW);
+
+	dig_mode(Z_STEP_PIN, OUTPUT);
+	dig_mode(Z_DIR_PIN, OUTPUT);
+	dig_mode(Z_ENABLE_PIN, OUTPUT);
+	dig_mode(Z_MIN_PIN, INPUT);
+#ifdef Z_MAZ_PIN
+	dig_mode(Z_MAZ_PIN, INPUT);
+#endif
+	dig_write(Z_ENABLE_PIN, LOW);
 }
