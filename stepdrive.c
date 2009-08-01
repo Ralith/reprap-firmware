60f83cdeef9a7b89f58f749f485264e4f32881d7
Ralith <ralith@gmail.com>
2009-07-31 17:14:29 -0700

    First attempt at enabling pin change interrupts for endstops

--- stepdrive.c
+++ stepdrive.c
@@ -5,11 +5,12 @@
 
 #include "digital.h"
 #include "uart.h"
-
+#include "util.h"
 #include "pins.h"
 
 void stepdrive_init(void)
 {
+	/* Configure and when necessary initialize stepper I/O */
 	dig_mode(X_STEP_PIN, OUTPUT);
 	dig_mode(X_DIR_PIN, OUTPUT);
 	dig_mode(X_ENABLE_PIN, OUTPUT);
@@ -37,6 +38,7 @@ void stepdrive_init(void)
 #endif
 	dig_write(Z_ENABLE_PIN, LOW);
 
+	/* Configure control timer */
 	TCCR1B |= _BV(CS01) | _BV(CS00) | /* Clock timer at F_CPU/64 */
 		_BV(WGM12);					/* Clear on Timer Compare mode */
 	/* TODO: Consider setting TOIE1 (overflow interrupt) */
@@ -44,14 +46,47 @@ void stepdrive_init(void)
 	OCR1A = 25000;				/* 100ms */
 
 	dig_mode(1, OUTPUT);
+
+	/* Establish pin change interrupts for endstops */
+	const ubyte endstops[] = {X_MIN_PIN, Y_MIN_PIN, Z_MIN_PIN,
+#ifdef X_MAX_PIN
+							  X_MAX_PIN,
+#endif
+#ifdef Y_MAX_PIN
+							  Y_MAX_PIN,
+#endif
+#ifdef Z_MAX_PIN
+							  Z_MAX_PIN,
+#endif
+							  0};
+	ubyte i;
+	for(i = 0; endstops[i] != 0; i++) {
+		if(endstops[i] <= PIN_PORTB_MAX) {
+			BSET(PCICR, PCIE0, 1);
+			BSET(PCMSK0, endstops[i] - PIN_PORTB_MIN, 1);
+		} else if(PIN_PORTD_MIN <= endstops[i] && endstops[i] <= PIN_PORTD_MAX) {
+			BSET(PCICR, PCIE1, 1);
+			BSET(PCMSK1, endstops[i] - PIN_PORTD_MIN, 1);
+		} else if(endstops[i] <= PIN_PORTC_MAX) { /* PORTC begins at PIN_PORTD_MAX + 1 */
+			BSET(PCICR, PCIE2, 1);
+			BSET(PCMSK2, endstops[i] - PIN_PORTC_MIN, 1);
+		} else if (PIN_PORTA_MIN <= endstops[i] && endstops[i] <= PIN_PORTA_MAX) {
+			/* PORTA is backwards for some reason, so we have to swap 7
+			 * 7 with 0, 6 with 1, etc. */
+			BSET(PCICR, PCIE3, 1);
+			BSET(PCMSK3, (7 - (endstops[i] - PIN_PORTA_MIN)), 1);
+		}
+	}
 }
 
+/* Timer interval expired */
 ISR(TIMER1_COMPA_vect) 
 {
 	dig_toggle(1);
 	uart_puts_P("tick\r\n");
 }
 
+/* Timer overflow; we missed a compare. */
 ISR(TIMER1_OVF_vect) 
 {
 	uart_puts_P("OVERFLOW");
