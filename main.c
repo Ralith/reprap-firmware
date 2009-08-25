88aadb8f4a8833530d1af41838e9eb1b81445da6
Ralith <ralith@gmail.com>
2009-08-25 16:38:49 -0700

    Added UART error handling and minor cleanup.

--- main.c
+++ main.c
@@ -20,19 +20,38 @@ int main(void)
 	/* Enable interrupts */
 	sei();
 
-	/* Indicate ready state to host software */
-	/* TODO: Use real hardware or software flow control */
-	uart_puts_P("start");
 #ifdef DEBUG_LED_PIN
 	dig_mode(DEBUG_LED_PIN, OUTPUT);
 	dig_write(DEBUG_LED_PIN, HIGH);
 #endif
+
+	/* Indicate ready state to host software */
+	/* TODO: Use real hardware or software flow control */
+	uart_puts_P("start\r\n");
 	uint16_t recv;
-	for(;; recv = uart_getc()) {
-		/* TODO: Handle UART errors */
-		if(recv != UART_NO_DATA) {
+	while(1) {
+		recv = uart_getc();
+		switch(recv >> 8) {
+		case UART_FRAME_ERROR >> 8:
+			uart_puts_P("ERROR: FRAME ERROR. Corrupted or misconfigured transmission\r\n");
+			break;
+			
+		case UART_OVERRUN_ERROR >> 8:
+			uart_puts_P("ERROR: OVERRUN. Receiving data too quickly\r\n");
+			break;
+			
+		case UART_BUFFER_OVERFLOW >> 8:
+			uart_puts_P("ERROR: BUFFER OVERFLOW. Not parsing fast enough\r\n");
+			break;
+
+		case UART_NO_DATA >> 8:
+			break;
+
+		default:
+		{
+			/* Got data */
 			static int8_t result;
-			result = gcode_parsec(recv);
+			result = gcode_parsec((char)recv);
 			switch(result) {
 			case GCODE_BLOCK_COMPLETE:
 				/* Indicate that we're ready for the next block. */
@@ -50,6 +69,7 @@ int main(void)
 				}
 				break;
 			}
+		}
 		}
 	}
 	
