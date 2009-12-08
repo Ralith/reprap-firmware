6de2cae5fae45ae965425c8f514c667054ff098c
Benjamin Saunders <besaunde@sfu.ca>
2009-12-07 18:50:00 -0800

    Added axis enum for readability and safety in indexing axis arrays.

--- types.h
+++ types.h
@@ -23,6 +23,12 @@ typedef enum {
 	PID_D
 } portid_t;
 
+typedef enum {
+	AXIS_X = 0,
+	AXIS_Y = 1,
+	AXIS_Z = 2
+} axis_t;
+
 /* See datasheet Figure 1-1 */
 #define PIN_MIN 1
 #define PIN_PORTB_MIN PIN_MIN
