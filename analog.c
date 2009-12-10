0419b1d6c45aba35b3dda0d72dd2a04b5a7a1d72
Benjamin Saunders <besaunde@sfu.ca>
2009-12-09 22:38:51 -0800

    Implemented blocking ADC sampling by channel.

--- analog.c
+++ analog.c
@@ -6,3 +6,17 @@ void adc_init() {
 	/* Enable ADC at sample rate (system clock)/8 */
 	ADCSRA |= BV(ADEN) | BV(ADPS0) | BV(ADPS1);
 }
+
+uint16_t adc_sample(uint8_t channel){
+	if(channel > 7) return 0;
+	uint8_t low_byte, high_byte;
+	ADMUX = channel;
+	ADCSRA |= (1 << ADSC);
+	while((ADCSRA & (1 << ADIF))  == 0){}; //Wait until the adc grab is complete
+	low_byte = ADCL;
+	high_byte = ADCH;
+	ADCSRA |= (1 << ADIF);
+	
+	/* Merge high/low bytes.*/
+	return (((uint16_t) high_byte) << 8) + low_byte;
+}
--- analog.h
+++ analog.h
@@ -1,1 +1,3 @@
 void adc_init();
+
+uint16_t adc_sample(uint8_t channel);
